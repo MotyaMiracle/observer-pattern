69c7587d4d8ce453dc2b23c6c1c44ae84c780bd4
motyamiracle@gmail.com <motyamiracle@gmail.com>
2022-02-09 23:23:25 +0300

    Try to succeed

---
+++ ClassLibraryObserver-Pattern/ConcreteObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryObserver_Pattern.Interfaces
+{
+    public class ConcreteObserver : IObserver
+    {
+        private int Counter;
+        public void Update()
+        {
+            Counter += 1;
+
+        }
+        public int GetCounter()
+        {
+            return Counter;
+        }
+    }
+}
--- observer-pattern/Program.cs
+++ observer-pattern/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ClassLibraryObserver_Pattern;
+using ClassLibraryObserver_Pattern.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,31 @@ namespace observer_pattern
     {
         static void Main(string[] args)
         {
+            ConcreteObserver observer1 = new ConcreteObserver();
+            ConcreteObserver observer2 = new ConcreteObserver();
+            ConcreteSubject subject = new ConcreteSubject();
+
+            subject.RegisterObserver(observer1);
+            subject.SetState("Sunny");
+
+            Console.WriteLine(observer1.GetCounter());
+            Console.WriteLine("Разрыв...");
+
+            subject.RegisterObserver(observer2);
+            subject.SetState("Cloudy");
+
+            Console.WriteLine(observer1.GetCounter());
+            Console.WriteLine(observer2.GetCounter());
+            Console.WriteLine("Разрыв...");
+
+            subject.RemoveObserver(observer2);
+            subject.SetState("Rain with thunderstorm");
+
+            Console.WriteLine(observer1.GetCounter());
+            Console.WriteLine(observer2.GetCounter());
+            Console.WriteLine("Разрыв...");
+
+            Console.ReadKey();
         }
     }
 }
